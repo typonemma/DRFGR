890b2eeefbac1f7e1dcbc3eab5fb4fffb4e43095
typonemma <glentong0@gmail.com>
2022-02-11 07:19:08 +0700

    DRFGR

---
+++ database/migrations/2022_02_09_104411_create_information_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInformationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('information', function (Blueprint $table) {
+            $table->id();
+            $table->integer('Total_Count_DAY');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('information');
+    }
+}
