418f95ae5fde87979e4431951723f499d4f35e70
Hael12122002 <hendralijaya@gmail.com>
2022-02-12 18:40:10 +0700

    adding templating engine to views

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends Controller
 {
     public function index()
     {
-        return view();
+        return view('login');
     }
 
     public function authenticate(StoreLogin $request)
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -164,9 +164,10 @@
     <body class="antialiased">
        <div class="login">
         <h1>Login</h1>
-            <form method="post">
-                <input type="text" name="u" placeholder="Username" required="required" />
-                <input type="password" name="p" placeholder="Password" required="required" />
+            <form method="post" action="{{ route('login.authenticate') }}">
+                @csrf
+                <input type="text" name="email" placeholder="Email" required="required" />
+                <input type="password" name="password" placeholder="Password" required="required" />
                 <button type="submit" class="btn btn-primary btn-block btn-large">login</button>
             </form>
         </div>
