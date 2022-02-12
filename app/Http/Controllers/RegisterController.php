d7e3b8bf5b22a3661fa4f3ca90dd561ac13f9e81
typonemma <glentong0@gmail.com>
2022-02-12 22:02:11 +0700

    Update views to controller

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -11,7 +11,7 @@ class RegisterController extends Controller
 {
     public function index()
     {
-        return view();
+        return view('register');
     }
 
     public function store(StoreRegister $request)
