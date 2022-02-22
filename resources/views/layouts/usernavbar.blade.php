f742fcec9112666f2c9beda2e0938ee576167784
typonemma <glentong0@gmail.com>
2022-02-22 23:26:17 +0700

    new navbar

--- resources/views/layouts/usernavbar.blade.php
+++ resources/views/layouts/usernavbar.blade.php
@@ -1,6 +1,5 @@
-<link rel="stylesheet" href="{{asset('/css/navbar.css')}}"
-
-
+<link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <!--Calling bootstrap-->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -9,6 +8,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
     <!--Calling Icon CSS-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="{{asset('/css/style.css')}}">
 
 <nav class="navbar navbar-expand-lg navbar-light ">
     <h1 class="navbar-header mr-4" href="#">Yokogawa</h1>
@@ -21,14 +21,14 @@
           <a class="nav-link" href="#">Home </a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Form
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">Goods Receive Form</a>
-            <a class="dropdown-item" href="#">Dispatch Request Form</a>
-          </div>
-        </li>
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Form
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Goods Receive Form</a>
+              <a class="dropdown-item" href="#">Dispatch Request Form</a>
+            </div>
+          </li>
       </ul>
     </div>
     <div class="logout my-2 my-lg-0">
