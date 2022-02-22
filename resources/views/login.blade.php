3f55ca271ac89f2ea730adbaaa48ebca6e7e0810
typonemma <glentong0@gmail.com>
2022-02-22 17:58:10 +0700

    add a href for loginuser to register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -22,8 +22,8 @@
                 <input type="text" name="email" placeholder="Email" required="required" />
                 <input type="password" name="password" placeholder="Password" required="required" />
                 <button type="submit" class="btn btn-primary btn-block btn-large">Login</button>
-
             </form>
+            
         </div>
     </body>
 </html>
