8abe943657e38b3a2e079e34d0b0ce196e0f1445
typonemma <glentong0@gmail.com>
2022-03-08 11:37:32 +0700

    add more button js

--- resources/views/formgr.blade.php
+++ resources/views/formgr.blade.php
@@ -10,6 +10,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         <link rel="stylesheet" href="{{asset('css/form.css')}}">
         <!-- Styles -->
 
@@ -61,7 +62,7 @@
   <div>
     <label class="desc" id="title1" for="Field2">Customer Name</label>
     <div>
-      <input id="Field2" name="customer_name" type="text" class="field text fn" value="" size="8" tabindex="1">
+      <input id="Field2" name="customer_name" type="text" class="field text fn" value="" size="8" tabindex="1" placeholder="Customer Name">
         @error('customer_name')
           {{ $message }}
         @enderror
@@ -71,7 +72,7 @@
   <div>
     <label class="desc" id="title1" for="Field3">Customer Adress</label>
     <div>
-      <input id="Field3" name="customer_address" type="text" class="field text fn" value="" size="8" tabindex="1">
+      <input id="Field3" name="customer_address" type="text" class="field text fn" value="" size="8" tabindex="1" placeholder="Customer Adress">
         @error('customer_address')
           {{ $message }}
         @enderror
@@ -81,7 +82,7 @@
   <div>
     <label class="desc" id="title1" for="Field4">Customer Fax or Telephone</label>
     <div>
-      <input id="Field4" name="customer_telephone" type="text" class="field text fn" value="" size="8" tabindex="1">
+      <input id="Field4" name="customer_telephone" type="text" class="field text fn" value="" size="8" tabindex="1" placeholder="Fax or Telephone">
         @error('customer_telephone')
           {{ $message }}
         @enderror
@@ -93,7 +94,7 @@
   <div>
     <label class="desc" id="title1" for="Field5">Contact Person</label>
     <div>
-      <input id="Field5" name="contact_person" type="text" class="field text fn" value="" size="8" tabindex="1">
+      <input id="Field5" name="contact_person" type="text" class="field text fn" value="" size="8" tabindex="1" placeholder="Contact Person">
       @error('contact_person')
           {{ $message }}
         @enderror
@@ -103,14 +104,14 @@
   <div>
     <label class="desc" id="title1" for="Field6">In date</label>
     <div>
-      <input id="Field6" name="in_date" type="text" class="field text fn" value="" size="8" tabindex="1">
+      <input id="Field6" name="in_date" type="text" class="field text fn" value="" size="8" tabindex="1" placeholder="In Date">
       @error('in_date')
           {{ $message }}
         @enderror
     </div>
   </div>
 
-  <div>
+  <div class="add">
     <label class="desc" id="title1" for="Field7">Nomor Model</label>
     <div>
       <input id="Field7" name="instrument_model" type="text" class="field text fn" value="" size="8" tabindex="1" placeholder="Instrument Models">
@@ -134,7 +135,7 @@
 
   <div>
     <div>
-      	<button type="submit" class="btn btn-block btn-large"><i class="bi bi-plus"></i>Add More</button>
+      	<button type="button" id="add_more" class="add-row btn btn-block btn-large "><i class="bi bi-plus"></i>Add More</button>
     </div>
   </div>
 
@@ -146,7 +147,7 @@
     </label>
 
     <div>
-      <textarea id="Field9" name="description" spellcheck="true" rows="10" cols="50" tabindex="4"></textarea>
+      <textarea id="Field9" name="description" spellcheck="true" rows="10" cols="50" tabindex="4" placeholder="Put your description...."></textarea>
       @error('description')
           {{ $message }}
         @enderror
@@ -161,7 +162,21 @@
   		<button type="submit" class="btn btn-block btn-large"><i class="bi bi-check-lg"></i>Submit</button>
     </div>
 	</div>
-
 </form>
+
+<script type="text/javascript">
+
+$(document).ready(function(){
+   $( ".add-row" ).click(function(){
+      var $clone = $( "div.add" ).first().clone();
+
+      $clone.insertBefore( ".add-row" );
+   });
+
+
+});
+</script>
+
+
     </body>
 </html>
