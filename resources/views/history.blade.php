72c5a244f27775e349ed017d44e0bf5aab236ca2
typonemma <glentong0@gmail.com>
2022-03-26 12:28:08 +0700

    date pick new

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 4 DatePicker</title>
+    <title>History</title>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
@@ -44,6 +44,6 @@
             uiLibrary: 'bootstrap4'
         });
     </script>
-    
+
 </body>
 </html>
