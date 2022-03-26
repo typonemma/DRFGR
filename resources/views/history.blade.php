d736c8aa6f77e75d35947d2aae5e17459c686b49
typonemma <glentong0@gmail.com>
2022-03-26 12:25:57 +0700

    date picker

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -9,17 +9,40 @@
     <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
 </head>
 @include('layouts.navbar')
-<body>
+<body class="antialiased" style="background-image: #EAD689;">
+      <header class="ml-4 mt-4 auto ">
+        <h2  style=" font-weight: bold; text-color:#2A363B;">History</h2>
+      </header>
+      <section class="statis mt-4 ">
+        <div class="row justify-content-md-center">
+          <div class="col-md-6 col-lg-11 mb-12 mb-lg-0">
+            <form action="#">
+              <input type="submit" value="History DRF" class="box  col-lg-12 text-left"  style="background:#2A363B; border-radius: 10px; height:100px; color:#EAD689; font-size:32px;" />
+            </form>
+          </div>
+        </div>
+      </section>
 
-    <input id="datepicker" width="276" />
+      <section class="statis mt-4 ">
+        <div class="row justify-content-md-center">
+            <div class="col-md-6 col-lg-11 mb-12 mb-lg-0" >
+            <form action="#">
+              <input type="submit" value="History GR" class="box  col-lg-12 text-left justify-content-center"  style="background:#2A363B; border-radius: 10px; height:100px; color:#EAD689; font-size:32px;" />
+            </form>
+          </div>
+        </div>
+      </section>
+
+      <div class="row justify-content-md-center">
+        <div class="col-md-6 col-lg-11 mb-12 mb-lg-0" >
+              <input id="datepicker" width="276" />
+        </div>
+      </div>
+
     <script>
         $('#datepicker').datepicker({
             uiLibrary: 'bootstrap4'
         });
-<<<<<<< HEAD
-        
-=======
->>>>>>> development
     </script>
 </body>
 </html>
