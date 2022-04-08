42b4d692cf25585bf97ffc998eaf1cb60c2bd809
Hael12122002 <hendralijaya@gmail.com>
2022-04-08 11:56:49 +0700

    push

--- resources/views/dashboardadmin.blade.php
+++ resources/views/dashboardadmin.blade.php
@@ -37,28 +37,28 @@
        <div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
          <div class="box  p-3" >
            <i class="uil-eye"></i>
-           <h3>0</h3>
+           <h3>{{ $ivspWeek }}</h3>
            <p class="lead">This Week GR</p>
          </div>
        </div>
        <div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
          <div class="box  p-3">
            <i class="uil-user"></i>
-           <h3>0</h3>
+           <h3>{{ $drfWeek }}</h3>
            <p class="lead">This Week drf</p>
          </div>
        </div>
        <div class="col-md-6 col-lg-3 mb-4 mb-md-0">
          <div class="box  p-3">
            <i class="uil-shopping-cart"></i>
-           <h3>0</h3>
+           <h3>{{ $ivspMonth }}</h3>
            <p class="lead">This Month GR</p>
          </div>
        </div>
        <div class="col-md-6 col-lg-3">
          <div class="box  p-3">
            <i class="uil-feedback"></i>
-           <h3>0</h3>
+           <h3>{{ $drfMonth }}</h3>
            <p class="lead">This Month drf</p>
          </div>
        </div>
